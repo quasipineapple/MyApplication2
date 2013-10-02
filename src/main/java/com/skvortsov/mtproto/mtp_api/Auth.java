8710b0aaddffe6a2792cdb00af165667b9f2d8bc
quasipineapple <skvorcov_s@mail.ru>
2013-10-02 23:12:07 +0400

    123

--- src/main/java/com/skvortsov/mtproto/mtp_api/Auth.java
+++ src/main/java/com/skvortsov/mtproto/mtp_api/Auth.java
@@ -11,10 +11,10 @@ public class Auth {
     public Method SendCode() throws CloneNotSupportedException {
 
         Method auth_sendCode = BookManager.getBook().getMethodByName("Auth.sendCode").clone();
-        auth_sendCode.getParamByName("phone_number").setData("");
+        auth_sendCode.getParamByName("phone_number").setData("+79056624155");
         auth_sendCode.getParamByName("sms_type").setData(0);
-        auth_sendCode.getParamByName("api_id").setData(0);
-        auth_sendCode.getParamByName("api_hash").setData("");
+        auth_sendCode.getParamByName("api_id").setData(1463);
+        auth_sendCode.getParamByName("api_hash").setData("437a55dcaee748fc1596a1bb5e6ca7db");
 
         return auth_sendCode;
     }
